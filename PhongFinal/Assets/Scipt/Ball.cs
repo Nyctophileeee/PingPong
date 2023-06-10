b93c72cc1c6a474b672c8fa9d33e7cc35ba73df8
Lester.M <102751769+Nyctophileeee@users.noreply.github.com>
2023-06-11 01:25:07 +0800

    add photon

--- PhongFinal/Assets/Scipt/Ball.cs
+++ PhongFinal/Assets/Scipt/Ball.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float speed = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(3, 2 * speed);
     }
 
     // Update is called once per frame
---
+++ PhongFinal/Assets/Scipt/ConToServer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
+
+public class ConToServer: MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
